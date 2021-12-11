a3f83cc2351eb77d6d03e161118cf77a24839927
David Buckley <david@davidbuckley.ca>
2021-12-11 12:06:13 -0500

    an update

--- aoc_utils/grid.py
+++ aoc_utils/grid.py
@@ -1,56 +1,72 @@
-class Grid:
-    def __init__(self, arr):
-        self.arr = arr
-        self.height = len(arr)
-        self.width = len(arr[0])
+from typing import List, TypeVar, Tuple, Union, Generic, Iterator, Callable
 
-    def __contains__(self, yx):
+S = TypeVar("S")
+T = TypeVar("T")
+
+
+class Grid(Generic[T]):
+    def __init__(self, arr: List[List[T]]) -> None:
+        self.arr = arr
+
+    @property
+    def height(self) -> int:
+        return len(self.arr)
+
+    @property
+    def width(self) -> int:
+        return len(self.arr[0])
+
+    def __contains__(self, yx: Tuple[int, int]) -> bool:
         y, x = yx
         return 0 <= y < self.height and 0 <= x < self.width
 
-    def __getitem__(self, yx):
-        if isinstance(yx, Tuple):
+    def __getitem__(self, yx: Union[Tuple[int, int], int]) -> Union[T, List[T]]:
+        if isinstance(yx, tuple):
             if yx not in self:
                 raise IndexError
             y, x = yx
             return self.arr[y][x]
-        elif not isinstance(yx, int): 
+        elif not isinstance(yx, int):
             raise TypeError
         return self.arr[yx]
 
+    def __iter__(self) -> Iterator[T]:
+        return self.row_major()
 
-    def __iter__(self):
-        return iter(self.arr)
-
-    def row_major_with_index(self):
+    def row_major_with_index(self) -> Iterator[Tuple[Tuple[int, int], T]]:
         for i in range(self.height):
             for j in range(self.width):
                 yield ((i, j), self.arr[i][j])
 
-    def row_major(self):
+    def row_major(self) -> Iterator[T]:
         for ((_, _), elem) in self.row_major_with_index():
             yield elem
 
-    def column_major_with_index(self):
+    def column_major_with_index(self) -> Iterator[Tuple[Tuple[int, int], T]]:
         for i in range(self.width):
             for j in range(self.height):
                 yield ((i, j), self.arr[j][i])
 
-    def column_major(self):
+    def column_major(self) -> Iterator[T]:
         for ((_, _), elem) in self.column_major_with_index():
             yield elem
 
-    def apply(self, func):
-        for row in self:
-            for item in row:
-                func(item)
+    def apply(self, func: Callable[[T], None]) -> None:
+        for item in self:
+            func(item)
 
-    def around_with_index(self, y, x):
+    def map(self, func: Callable[[T], S]) -> "Grid[S]":
+        return Grid([[func(item) for item in row] for row in self.arr])
+
+    def around_with_index(
+        self, yx: Tuple[int, int]
+    ) -> Iterator[Tuple[Tuple[int, int], T]]:
+        y, x = yx
         for i in range(-1, 2):
             for j in range(-1, 2):
-                if (y+i, x+j) in self:
-                    yield ((y+i, x+j), self.arr[y+i][x+j])
+                if (y + i, x + j) in self:
+                    yield ((y + i, x + j), self.arr[y + i][x + j])
 
-    def around(self, y, x):
-        for ((_, _), elem) in self.around_with_index(y, x):
+    def around(self, yx: Tuple[int, int]) -> Iterator[T]:
+        for ((_, _), elem) in self.around_with_index(yx):
             yield elem
