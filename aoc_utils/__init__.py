2d0c01f7529a62470c0bff4a4721ecafd9c8cfc8
David Buckley <david@davidbuckley.ca>
2022-12-19 12:57:22 -0500

    add bfs

--- aoc_utils/__init__.py
+++ aoc_utils/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.0"
 
 from aoc_utils.grid import Grid, Direction
-from aoc_utils.graph import Graph, LazyGraph
+from aoc_utils.graph import Graph, LazyGraph, UnweightedLazyGraph
 from aoc_utils.shunting_yard import ShuntingYard
 from aoc_utils.collections import *
 from aoc_utils.range import Range
@@ -10,7 +10,7 @@ from aoc_utils.point import Point, Point3
 
 __all__ = [
     'Grid', 'Direction',
-    'Graph', 'LazyGraph',
+    'Graph', 'LazyGraph', 'UnweightedLazyGraph',
     'ShuntingYard',
     'Range',
     'maxn', 'minn', 'take', 'chunk', 'groups',
--- aoc_utils/graph.py
+++ aoc_utils/graph.py
@@ -1,12 +1,12 @@
 import abc
-from collections import defaultdict
+from collections import defaultdict, deque
 try:
     from functools import cache
 except ImportError:
     from functools import lru_cache
     cache = lru_cache(None)
 import heapq
-from typing import TypeVar, Generic, Iterable, Callable, Tuple, Set, Iterator, Union
+from typing import Any, TypeVar, Generic, Iterable, Callable, Tuple, Set, Iterator
 
 T = TypeVar("T")
 
@@ -30,10 +30,30 @@ class Dijkstra(Generic[T]):
         path.reverse()
         return path
 
-
 class NodeGraph(Generic[T], abc.ABC):
     @abc.abstractmethod
-    def edges(self, node: T) -> Iterable[Tuple[int, T]]:
+    def edges(self, node: T) -> Iterable[T]:
+        ...
+
+    def bfs(self, start: T, visit: Callable[[T], Any]):
+        history = set([start])
+        queue = deque([start])
+        while queue:
+            node = queue.pop()
+            if not visit(node):
+                continue
+            for other_node in self.edges(node):
+                if other_node not in history:
+                    history.add(other_node)
+                    queue.appendleft(other_node)
+
+
+class WeightedGraph(Generic[T], NodeGraph[T], abc.ABC):
+    def edges(self, node: T) -> Iterable[T]:
+        yield from (node for _, node in self.edges_with_weight(node))
+
+    @abc.abstractmethod
+    def edges_with_weight(self, node: T) -> Iterable[Tuple[int, T]]:
         ...
 
     def dijkstra(self, start: T) -> Dijkstra:
@@ -48,7 +68,7 @@ class NodeGraph(Generic[T], abc.ABC):
             if u in history:
                 continue
             history.add(u)
-            for cost, v in self.edges(u):
+            for cost, v in self.edges_with_weight(u):
                 if v in history:
                     continue
 
@@ -59,7 +79,8 @@ class NodeGraph(Generic[T], abc.ABC):
                     heapq.heappush(pq, (alt, v))
         return Dijkstra(start, dist, prev)
 
-class Graph(NodeGraph[T]):
+
+class Graph(WeightedGraph[T]):
     def __init__(self):
         self._edges = defaultdict(set)
         super().__init__()
@@ -67,17 +88,21 @@ class Graph(NodeGraph[T]):
     def add_edge(self, p: T, q: T, weight=1):
         self._edges[p].add((weight, q))
 
-    def edges(self, node: T) -> Set[Tuple[int, T]]:
+    def edges_with_weight(self, node: T) -> Set[Tuple[int, T]]:
         return self._edges[node]
 
 
-class LazyGraph(NodeGraph[T]):
-    def __init__(self, neighbour_fn: Callable[[T], Iterable[Union[Tuple[int, T], T]]]):
+class LazyGraph(WeightedGraph[T]):
+    def __init__(self, neighbour_fn: Callable[[T], Iterable[Tuple[int, T]]]):
         self.neighbour_fn = neighbour_fn
 
-    def edges(self, node: T) -> Iterator[Tuple[int, T]]:
-        for n in self.neighbour_fn(node):
-            if isinstance(n, tuple):
-                yield n
-            else:
-                yield 1, n
+    def edges_with_weight(self, node: T) -> Iterator[Tuple[int, T]]:
+        yield from self.neighbour_fn(node)
+
+
+class UnweightedLazyGraph(NodeGraph[T]):
+    def __init__(self, neighbour_fn: Callable[[T], Iterable[T]]):
+        self.neighbour_fn = neighbour_fn
+
+    def edges(self, node: T) -> Iterator[T]:
+        yield from self.neighbour_fn(node)
